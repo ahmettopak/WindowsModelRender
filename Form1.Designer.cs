2597cb6af9f5bcc234d244b583788270f0cface7
Ahmet TOPAK <75997899+ahmettopak@users.noreply.github.com>
2023-08-04 04:02:19 +0300

    Model transfrom change and other model add

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -25,16 +25,46 @@
         private void InitializeComponent() {
             this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
             this.userControl11 = new _3DModelRender.UserControl1();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // elementHost1
             // 
-            this.elementHost1.Location = new System.Drawing.Point(12, 12);
+            this.elementHost1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.elementHost1.Location = new System.Drawing.Point(3, 3);
             this.elementHost1.Name = "elementHost1";
-            this.elementHost1.Size = new System.Drawing.Size(789, 426);
+            this.elementHost1.Size = new System.Drawing.Size(620, 444);
             this.elementHost1.TabIndex = 0;
             this.elementHost1.Text = "elementHost1";
             this.elementHost1.Child = this.userControl11;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 78.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 21.75F));
+            this.tableLayoutPanel1.Controls.Add(this.elementHost1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(629, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
@@ -42,10 +72,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.elementHost1);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -54,6 +85,8 @@
 
         private System.Windows.Forms.Integration.ElementHost elementHost1;
         private UserControl1 userControl11;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,26 +16,56 @@ namespace _3DModelRender {
             LoadAndShowObjModel();
         }
 
+        Model3DGroup modelGroup = new Model3DGroup();
         private void LoadAndShowObjModel() {
             var importer = new AssimpContext();
 
             string objFilePath = "C:/Users/trahm/Desktop/Projects/UnitySimulationMaker/Assets/TMRModels/govde.obj";
+           
+            string objFilePath1 = "C:/Users/trahm/Desktop/Projects/UnitySimulationMaker/Assets/TMRModels/bilek.obj";
 
             Scene scene = importer.ImportFile(objFilePath, PostProcessSteps.Triangulate | PostProcessSteps.FlipUVs);
 
+            Scene scene1 = importer.ImportFile(objFilePath1, PostProcessSteps.Triangulate | PostProcessSteps.FlipUVs);
             if (scene == null || scene.RootNode == null) {
                 MessageBox.Show("OBJ dosyası yüklenirken bir hata oluştu.");
                 return;
             }
 
-            Model3DGroup modelGroup = new Model3DGroup();
 
             foreach (var mesh in scene.Meshes) {
                 var positions = new Point3DCollection();
                 var normals = new Vector3DCollection();
                 var indices = new Int32Collection();
 
-                // Mesh'in vertex ve index bilgilerini alın.
+                foreach (var vertex in mesh.Vertices) {
+                    positions.Add(new Point3D(vertex.X, vertex.Y, vertex.Z));
+                    normals.Add(new System.Windows.Media.Media3D.Vector3D(vertex.X, vertex.Y, vertex.Z));
+                }
+
+                foreach (var face in mesh.Faces) {
+                    foreach (var index in face.Indices) {
+                        indices.Add(index);
+                    }
+                }
+
+                var meshGeometry3D = new MeshGeometry3D() {
+                    Positions = positions,
+                    Normals = normals,
+                    TriangleIndices = indices,
+                    
+                };
+                
+                var geometryModel3D = new GeometryModel3D(meshGeometry3D, new DiffuseMaterial(System.Windows.Media.Brushes.Gray));
+              
+                modelGroup.Children.Add(geometryModel3D);
+            }
+
+            foreach (var mesh in scene1.Meshes) {
+                var positions = new Point3DCollection();
+                var normals = new Vector3DCollection();
+                var indices = new Int32Collection();
+
                 foreach (var vertex in mesh.Vertices) {
                     positions.Add(new Point3D(vertex.X, vertex.Y, vertex.Z));
                     normals.Add(new System.Windows.Media.Media3D.Vector3D(vertex.X, vertex.Y, vertex.Z));
@@ -54,9 +84,30 @@ namespace _3DModelRender {
                 };
 
                 var geometryModel3D = new GeometryModel3D(meshGeometry3D, new DiffuseMaterial(System.Windows.Media.Brushes.Gray));
+                
+                // Apply transformations to the model here
+                var transformGroup = new Transform3DGroup();
+
+                // Example: Translation
+                var translation = new TranslateTransform3D(0, 50, 0); // Move 10 units along X-axis
+                transformGroup.Children.Add(translation);
+
+                // Example: Rotation
+                var rotation = new AxisAngleRotation3D(new System.Windows.Media.Media3D.Vector3D(0, 1, 0), 45); // Rotate 45 degrees around Y-axis
+                var rotationTransform = new RotateTransform3D(rotation);
+                transformGroup.Children.Add(rotationTransform);
+
+                // Apply the transform group to the model
+                geometryModel3D.Transform = transformGroup;
                 modelGroup.Children.Add(geometryModel3D);
             }
             userControl11.modelVisual3D.Content = modelGroup;
+            
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e) {
             
         }
     }
